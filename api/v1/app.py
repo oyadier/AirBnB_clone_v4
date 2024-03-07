9b8eca97729fdc2f40f29e9d4472780ae1453dea
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-03-07 12:05:05 +0000

    Formatting

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,11 @@ from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
+
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -30,10 +31,12 @@ def not_found(error):
     """
     return make_response(jsonify({'error': "Not found"}), 404)
 
+
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
     'uiversion': 3
 }
+
 
 Swagger(app)
 
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,14 @@
 $(document).ready(init);
 
 function init () {
-    const obj_amenity = {};
-    $('.amenities .popover input').change(function () {
-        if ($(this).is(':checked')) {
-            obj_amenity[$(this).attr('data-name')] = $(this).attr('data-id');
-        }else if ($(this).is(':not(:checked)')) {
-            delete obj_amenity[$(this).attr('data-name')];
-        }
-        const ame_obj = Object.keys(obj_amenity);
-        $('.amenities h4').text(ame_obj.sort().join(', '));
-    });
+  const objAmenity = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      objAmenity[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete objAmenity[$(this).attr('data-name')];
+    }
+    const ameObj = Object.keys(objAmenity);
+    $('.amenities h4').text(ameObj.sort().join(', '));
+  });
 }
